8099f3f1f90eb1d7a55ebafaa3bddeabe7b6a7b9
Petri-Johan Last <petri.last@sourcegraph.com>
2024-03-27 09:28:01 +0200

    Rework cody integration branch

--- crates/zed/src/main.rs
+++ crates/zed/src/main.rs
@@ -149,6 +149,7 @@ fn main() {
         let mut languages =
             LanguageRegistry::new(login_shell_env_loaded, cx.background_executor().clone());
         let copilot_language_server_id = languages.next_language_server_id();
+        let cody_language_server_id = languages.next_language_server_id();
         languages.set_language_server_download_dir(paths::LANGUAGES_DIR.clone());
         let languages = Arc::new(languages);
         let node_runtime = RealNodeRuntime::new(http.clone());
@@ -171,6 +172,12 @@ fn main() {
         diagnostics::init(cx);
         copilot::init(
             copilot_language_server_id,
+            http.clone(),
+            node_runtime.clone(),
+            cx,
+        );
+        cody::init(
+            cody_language_server_id,
             http.clone(),
             node_runtime.clone(),
             cx,
